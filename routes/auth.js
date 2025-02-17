adc5b8d47f06efd2aa68090992dfa5d087f54602
Kwasi Adabo Boakye <Kwasadaboboakye@gmail.com>
2025-02-17 11:37:09 +0000

    nodeModules

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Joi = require('joi');
 const router = express.Router();
 const User = require('../models/users');
-//const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 
